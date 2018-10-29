a34fb69049c9a028e3a65fc3896fbbc1c66a32a7
Sam Nakale <samnakale@yahoo.co.uk>
2018-10-29 14:33:02 +0200

    Application cleanup. Some UI views were removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,6 @@ const routes: Routes = [
     canActivate: [LoginRouteGuard],
     loadChildren: './components/admin/admin.component.module#AdminComponentModule'
   },
-  { 
-    path: 'home', 
-    canActivate: [LoginRouteGuard],
-    loadChildren: './components/home/home.component.module#HomeComponentModule'
-  },
   {
     path: 'login', 
     loadChildren: './components/login/login.component.module#LoginComponentModule',
@@ -21,11 +16,6 @@ const routes: Routes = [
     path: 'academic', 
     canActivate: [LoginRouteGuard],
     loadChildren: './components/academic/academic.component.module#AcademicComponentModule'
-  },
-  { 
-    path: 'students', 
-    canActivate: [LoginRouteGuard],
-    loadChildren: './components/students/students.component.module#StudentsComponentModule'
   },
   { 
     path: '', 
